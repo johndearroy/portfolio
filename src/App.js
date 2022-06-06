ffbd3cf2d5c372461a5dee713e048c841a0aeb54
Dear Roy <royatrangpur@gmail.com>
2022-06-06 10:52:42 +0600

    Everthing is done except the Resume section

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
 import Hero from "./components/basic/Hero";
-import Preloader from "./components/basic/preloader";
-import BackToTopButton from "./components/basic/backToTop";
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
       <Hero/>
       <Content/>
       <Footer/>
-      <Preloader/>
-      <BackToTopButton/>
     </>
   );
 }
--- src/components/basic/backToTop.jsx
+++
@@ -1,9 +0,0 @@
-const BackToTopButton = () => {
-  return (
-    <a href="#" className="back-to-top d-flex align-items-center justify-content-center">
-      <i className="bi bi-arrow-up-short"/>
-    </a>
-  )
-}
-
-export default BackToTopButton;
